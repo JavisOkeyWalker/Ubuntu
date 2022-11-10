41b59efc7445274b6013b7e07e1f73fe3ee8a705
Ubuntu <okey-walker.2@wright.edu>
2022-11-10 14:26:32 +0000

    lab10

---
+++ Lab10/repeat.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+	    char userText[20];
+	        printf("Enter text: \n");
+		    scanf("%s", &userText);  
+		        printf("Text = %s",userText);
+			    return 0;
+}
